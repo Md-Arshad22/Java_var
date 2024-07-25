0f0f09e030728a55344a36dd71895af517b855a2
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:31:33 +0530

    Update arr1.java

--- arr1.java
+++ arr1.java
@@ -1,1 +1,15 @@
+import java.util.List;
+public class arr1 {
 
+  public static void main(String[] args) {
+    // Sample data (replace with your actual data)
+    List<String> names = List.of("Apple", "Banana", "Cherry","orange");
+    int[] quantities = {10, 20, 30,40};
+
+    System.out.println("Fruits\tQuantity");
+    System.out.println("-------\t-------");
+    for (int i = 0; i < names.size(); i++) {
+      System.out.printf("%-10s\t%d\n", names.get(i), quantities[i]);
+    }
+  }
+}
