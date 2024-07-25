a96f281b194e0450283e80c767fd7839950202af
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:30:57 +0530

    Update arr.java

--- arr.java
+++ arr.java
@@ -4,6 +4,13 @@ public class arr {
   public static void main(String[] args) {
 
     ArrayList<String> list = new ArrayList<>();
-    
+    list.add("Abdul Mustafa");
+    list.add("Arshad Raza");
+    list.add("Arshad");
+
+    //System.out.println("Students are: "+ list.get(1));
+    for (String Students:list){
+      System.out.println(Students);
+    }
   }
 }
