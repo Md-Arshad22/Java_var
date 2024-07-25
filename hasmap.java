3db531c7b3e09439e35532ed5fea13cf3d40ab7d
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:32:48 +0530

    Update hasmap.java

--- hasmap.java
+++ hasmap.java
@@ -1,1 +1,14 @@
+import java.util.HashMap;
+public class hasmap {
+    public static void main(String[] args) {
 
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Zaheer");
+        map.put(2, "Arshad");
+        map.put(3, "Ahmed");
+        System.out.println("name : " + map);
+        for (Integer key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }   
+    }
+}
