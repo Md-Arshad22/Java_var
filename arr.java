c0e85e502d0558186f98991badae9be625500130
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:28:31 +0530

    Update arr.java

--- arr.java
+++ arr.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 
 public class arr {
-  public static void main(String[] args) 
+  public static void main(String[] args) {
+
+    ArrayList<String> list = new ArrayList<>();
+    
+  }
 }
