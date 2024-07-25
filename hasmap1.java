9433f0654c0b953422f851fe50b508a323a7a116
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:33:11 +0530

    Update hasmap1.java

--- hasmap1.java
+++ hasmap1.java
@@ -1,1 +1,15 @@
+import java.util.HashMap;
+public class hasmap1 {
+    public static void main(String[] args) {
 
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Md", "Arshad");
+        map.put("Md", "Zaheer");
+        map.put("Zaheer", "Arshad");
+
+        System.out.println("name : " + map);
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }   
+    }
+}
