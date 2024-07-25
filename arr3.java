b89d87433559838acd1c96f8e469a40fc7a84b00
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:54:35 +0530

    Update arr3.java

--- arr3.java
+++ arr3.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class arr3 {
     public static void main(String[] args) {
-                                                        
+                                                      
         ArrayList<String> list=new ArrayList<>();
         list.add("Zaheer");
         list.add("Arshad");
