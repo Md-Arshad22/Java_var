9b04ee111e74119525b8dcd28a88c653e16ff70f
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 16:47:49 +0530

    Update hasmap.java

--- hasmap.java
+++ hasmap.java
@@ -2,7 +2,6 @@ import java.util.HashMap;
 
 public class hasmap {
     public static void main(String[] args) {
-        
         HashMap<Integer, String> map = new HashMap<>();
         map.put(1, "Zaheer");
         map.put(2, "Arshad");
