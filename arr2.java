adc77fa40bf7f05f1a80a026db8c15cc4779587d
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-07-25 09:41:35 +0530

    Update arr2.java

--- arr2.java
+++ arr2.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class arr2 {
     public static void main(String[] args) {
         ArrayList<Object> list = new ArrayList<>();
-   
         list.add(1);
         list.add("two");
         list.add(3);
